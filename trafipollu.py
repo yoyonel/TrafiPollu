00f54cbd47d8b5b904479686d6f4f04f02402d68
yoyonel@hotmail.com <yoyonel@hotmail.com>
2015-05-29 09:14:19 +0200

    Reformat code with Sublime IDE (& plugs)

--- trafipollu.py
+++ trafipollu.py
@@ -1,54 +1,42 @@
-__author__ = 'atty'
+"""Summary
+
+Returns:
+    TYPE: Description
+"""
 
 from trafipolluImp import TrafiPolluImp
 
 
 class ITrafiPollu(TrafiPolluImp):
-    """
-
-    """
+    """"""
 
     def __init__(self, iface, dlg):
         """
-
+        Args:
+            iface (TYPE): Description
+            dlg (TYPE): Description
         """
         super(ITrafiPollu, self).__init__(iface, dlg)
 
     def init(self):
-        """
-
-        """
         self._init_signals_()
 
     def update(self):
-        """
-
-        """
         pass
 
     def enable(self):
-        """
-
-        """
         self._enable_trafipollu_()
 
     def disable(self):
-        """
-
-        """
         self._disable_trafipollu_()
 
     def get_dlg(self):
-        """
+        """Summary
 
-        :return:
-
+        Returns:
+            TYPE: Description
         """
         return self.dlg
-
-
-class TrafiPollu:
-    pass
 
 
 TrafiPollu = ITrafiPollu
