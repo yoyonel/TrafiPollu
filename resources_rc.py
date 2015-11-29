8bc28f2a857a2e4a6ab43b855787c0242a12b267
yoyonel <yoyonel@hotmail.com>
2015-11-29 12:46:39 +0100

    pyxb parser (generate). ressources_rc (generate)

--- resources_rc.py
+++ resources_rc.py
@@ -2,8 +2,8 @@
 
 # Resource object code
 #
-# Created: dim. nov. 29 02:14:14 2015
-# by: The Resource Compiler for PyQt (Qt v4.8.6)
+# Created: dim. nov. 29 12:44:31 2015
+#      by: The Resource Compiler for PyQt (Qt v4.8.6)
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -106,13 +106,10 @@ qt_resource_struct = "\
 \x00\x00\x00\x4a\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
 "
 
-
 def qInitResources():
     QtCore.qRegisterResourceData(0x01, qt_resource_struct, qt_resource_name, qt_resource_data)
 
-
 def qCleanupResources():
     QtCore.qUnregisterResourceData(0x01, qt_resource_struct, qt_resource_name, qt_resource_data)
-
 
 qInitResources()
